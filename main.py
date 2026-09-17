283aceff58cc32d0500e9589bafca7b791b3ba5a
nabila sahderina <nabilasahderina@gmail.com>
2026-09-17 21:25:13 +0700

    Initial commit: Tambahkan fungsi hitung total hasil panen

---
+++ main.py
@@ -0,0 +1,4 @@
+def hitung_total_panen(daftar_panen): return sum(daftar_panen)
+panen_padi = [120, 150, 135, 160]
+total = hitung_total_panen(panen_padi)
+print(f"Total Hasil Panen Padi: {total} kg")
