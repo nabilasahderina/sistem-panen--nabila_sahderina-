c6904ea9cba392d5326b60e87835e4acc22450c7
nabila sahderina <nabilasahderina@gmail.com>
2026-09-17 21:25:56 +0700

    Feat: Tambahkan fungsi perhitungan diskon panen

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ def hitung_total_panen(daftar_panen): return sum(daftar_panen)
 panen_padi = [120, 150, 135, 160]
 total = hitung_total_panen(panen_padi)
 print(f"Total Hasil Panen Padi: {total} kg")
+def hitung_diskon(total, persen): return total * (persen / 100)
