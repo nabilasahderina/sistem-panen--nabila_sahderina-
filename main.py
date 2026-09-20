f44329e5446bfc035edeea2226364a4120128afc
nabilasahderina <nabilasahderina@gmail.com>
2026-09-20 11:22:33 +0700

    Feat: Tambahkan fungsi penjualan

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ panen_padi = [120, 150, 135, 160]
 total = hitung_total_panen(panen_padi)
 print(f"Total Hasil Panen Padi: {total} kg")
 def hitung_diskon(total, persen): return total * (persen / 100)
+def hitung_penjualan(harga, jumlah):
+    return harga * jumlah
